694552326ea16a90148bd08170bab7cbfc8d0844
Dhruva Venkat <dhruva735@gmail.com>
2018-08-31 13:46:41 -0400

    Mostly finished the renderer, needs more UI integration`

--- Renderer.py
+++ Renderer.py
@@ -1,5 +1,43 @@
 from UI import *
-
 UI.mainloop()
 
 root = Tk()
+
+Image = Canvas(root, height = UI.winsize, width = UI.winsize)
+Image.pack()
+
+for x1 in range(0, UI.winsize+1):
+    x = (x1-(UI.winsize/2)) / UI.scale
+
+    for y1 in range(0, UI.winsize+1):
+        y = (y1-(UI.winsize/2)) / UI.scale
+
+        for i in range(int(UI.C_min), int(UI.C_max)):
+
+            z = complex(x, y) ** 2 + i
+
+            count = 0
+
+            while count < UI.Iter:
+
+                z = z**2 + i
+
+                count += 1
+
+                if abs(z) >= 10000:
+                    count = UI.Iter
+
+            if abs(z) <= 0.5:
+                Image.create_rectangle((x1, y1, x1 + 1, y1 + 1), fill="black")
+            
+            if 0.5 < abs(z) <= 1:
+                Image.create_rectangle((x1, y1, x1 + 1, y1 + 1), fill="green")
+
+            if 1 < abs(z) <= 2:
+                Image.create_rectangle((x1, y1, x1 + 1, y1 + 1), fill="blue")
+
+            if 2 < abs(z) <= 100:
+                Image.create_rectangle((x1, y1, x1 + 1, y1 + 1), fill="orange")
+
+root.mainloop()
+
--- UI.py
+++ UI.py
@@ -38,19 +38,74 @@ class create_UI(Tk):
         self.Scale_label.grid(row=5, column=0)
         self.Scale_value.grid(row=5, column=1)
 
+        #Window
+        self.Window_label = Label(self, text = "Window")
+        self.Win_value = Entry(self)
+        self.Window_label.grid(row = 6, column = 0)
+        self.Win_value.grid(row = 6, column = 1)
+
         #Enter
         self.Get = Button(self, text = "Enter", command = self.Enter)
-        self.Get.grid(row = 6, column = 1)
+        self.Get.grid(row = 7, column = 1)
 
         #Quit
         self.Quit = Button(self, text = "Quit", command = quit)
-        self.Quit.grid(row = 6, column = 0)
+        self.Quit.grid(row = 7, column = 0)
 
     def Enter(self):
-        print(self.C_min.get())
-        print(self.C_max.get())
-        print(self.Iterations.get())
-        print(self.Scale_value.get())
-        print(complex(self.Z_value.get()))
+
+        print("=" * 100)
+        print("Fractal Information:")
+
+        if len(self.C_min.get()) == 0:
+            print("You have entered no value for the Min, the default is -10")
+            self.C_min = -10
+        elif len(self.C_min.get()) > 0:
+            self.C_min = (self.C_min.get())
+            print("C Min: {}".format(self.C_min))
+
+        if len(self.C_max.get()) == 0:
+            print("You have entered no value for the Max, the default is 10")
+            self.C_max = 10
+        elif len(self.C_max.get()) > 0:
+            self.C_max = int(self.C_max.get())
+            print("C Max: {}".format(self.C_max))
+
+        if len(self.Iterations.get()) == 0:
+            print("You have entered no value for the Iterations, the default is 10")
+            self.Iter = 10
+        elif len(self.Iterations.get()) > 0:
+            self.Iter = int(self.Iterations.get())
+            print("Iterations: {}".format(self.Iter))
+
+        if len(self.Scale_value.get()) == 0:
+            print("You have entered no value for the Scale, the default is 100")
+            self.scale = 100
+        elif len(self.Scale_value.get()) > 0:
+            self.scale = int(self.Scale_value.get())
+            print("Scale: {}".format(self.scale))
+
+        if len(self.Z_value.get()) == 0:
+            print("You have entered no value for the Z, the default is 0")
+            self.Z = complex(0)
+        elif len(self.Z_value.get()) > 0:
+            try:
+                self.Z = complex(self.Z_value.get())
+                print("Z: {}".format(self.Z))
+            except:
+                raise ValueError("Please enter a complex number (0 + 4j)")
+
+        if len(self.Win_value.get()) == 0:
+            print("You have entered no value for the Window, the default is 400")
+            self.winsize = 400
+        elif len(self.Win_value.get()) > 0:
+            self.winsize = int(self.Win_value.get())
+            print("Window: {}".format(self.winsize))
+
+        print("=" * 100)
+
+        UI.quit()
+        UI.destroy()
+
 
 UI = create_UI()
