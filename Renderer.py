75195682f8cc00fbb5e391558de081638857bfc3
Dhruva Venkat <dhruva735@gmail.com>
2018-08-31 20:05:21 -0400

    Finished color palette, will add more in the future

--- Renderer.py
+++ Renderer.py
@@ -13,21 +13,35 @@ for x1 in range(0, UI.winsize+1):
     for y1 in range(0, UI.winsize+1):
         y = ((y1-(UI.winsize/2)) / UI.scale) + (UI.y_offset/UI.scale)
 
-        z = 0**UI.Z + complex(x, y)
+        z = (1-0)**UI.Z + complex(x, y)
 
+        iters = 0
         count = 0
 
         while count < UI.Iter:
 
-            z = z**UI.Z + complex(x, y)
+            z = (1-z)**UI.Z + complex(x, y)
 
             count += 1
+            iters += 1
+
+            if abs(z) > 2:
+                count = UI.Iter
+
             if abs(z) >= 10000:
                 count = UI.Iter
 
+        factor = (iters/UI.Iter)**(1/2)
+        intensity = int(round(UI.Iter*iters, 2))
+
+        if abs(z) > 4:
+            Image.create_rectangle(x1, y1, x1 + 1, y1 + 1, fill="black",outline="#%02x%02x%02x" % (0,0,0))
+
+        if abs(z) > 2:
+            Image.create_rectangle(x1, y1, x1 + 1, y1 + 1, fill="black", outline="#%02x%02x%02x" % (intensity,intensity,intensity))
+
         if abs(z) <= 2:
             Image.create_rectangle(x1, y1, x1 + 1, y1 + 1, fill="black", outline="#%02x%02x%02x" % (0,0,0))
-
 
 root.mainloop()
 
